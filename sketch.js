2e3ac976cabd060ec92360798e7d0d177ddb183d
virginia-nelson <vmn6772@nyu.edu>
2024-12-03 18:26:36 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+  createCanvas(400,400);
+}
+
+function draw(){
+  background(220);
+}
+
+//goal for this session: get the user to shoot bullets to kill enemies coming towards them
+//enemies will come from the top of the screen for now
+
+
+
