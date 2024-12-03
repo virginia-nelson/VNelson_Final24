a3a773eb4e80f6713624cebd96eae5abfbd7696b
virginia-nelson <vmn6772@nyu.edu>
2024-12-03 18:42:40 -0500

    setting up variables

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
+let bullets = [];//array to store bullets in
 function setup(){
   createCanvas(400,400);
 }
 
 function draw(){
   background(220);
+  circle(mouseX,height-50,25);//drawing the player (a circle)
+}
+
+function mousePressed(){
+  let bullet = {}
 }
 
 //goal for this session: get the user to shoot bullets to kill enemies coming towards them
